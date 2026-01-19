1ee4485db9730e287d54e6ac36ca150a4924d9b7
ifluency <152925865+ifluency@users.noreply.github.com>
2026-01-19 10:40:58 -0300

    Update __init__.py

--- parser/__init__.py
+++ parser/__init__.py
@@ -1,6 +1,18 @@
+# parser/__init__.py
+# -*- coding: utf-8 -*-
+
 from .parser import (
     process_pdf_bytes,
     process_pdf_bytes_debug,
     debug_dump,
     validate_extraction,
+    build_memoria_calculo_pdf,
 )
+
+__all__ = [
+    "process_pdf_bytes",
+    "process_pdf_bytes_debug",
+    "debug_dump",
+    "validate_extraction",
+    "build_memoria_calculo_pdf",
+]
