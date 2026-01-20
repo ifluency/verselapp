633bad0e7b8900da710744e9942cdb065548c2a2
ifluency <152925865+ifluency@users.noreply.github.com>
2026-01-20 11:39:08 -0300

    Update generate.py

--- api/generate.py
+++ api/generate.py
@@ -12,6 +12,7 @@ from parser.parser import (
     build_excel_bytes,
     build_memoria_calculo_pdf_bytes,
     build_pdf_tabela_comparativa_bytes,
+    PdfIncompatibilityError,
 )
 
 
@@ -82,6 +83,9 @@ class handler(BaseHTTPRequestHandler):
             self.send_header("Content-Length", str(len(zip_bytes)))
             self.end_headers()
             self.wfile.write(zip_bytes)
+
+        except PdfIncompatibilityError as e:
+            self._send_text(400, str(e))
 
         except Exception as e:
             tb = traceback.format_exc()
